f1b811f2954a9773ba12bd572a76d769cf00a510
Savas <savastyludf@hotmail.com>
2022-12-05 22:50:12 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "resource_name" {
+  default="savasrg"
+}
+
+variable "location" {
+    default = "West US"
+}
+variable "azurerm_storage_account" {
+  default = "savasstg55"
+}
+variable "vm_name" {
+    default = "savasvm"
+  
+}
+variable "user_name" {
+    default = "savas5353"
+  
+}
+variable "user_password" {
+    default = "Kis9223$55Kis9223$55"
+  
+}
